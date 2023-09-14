0f0e42b1822fd07cf2b47d41c9ef777598b14240
PataninThongsongsom <s6404062660011@email.kmutnb.ac.th>
2023-09-14 17:54:25 +0700

    add line and freehand can changes size of line

--- js/Custom.js
+++ js/Custom.js
@@ -1,14 +1,17 @@
-        var canvas = document.getElementById("myCanvas");
-        var ctx = canvas.getContext("2d");
-        var currentTool = "none"; // Default: no drawing
-        var currentColor = "#000000"; // Default color: black
-        var isDrawing = false;
-
+        let canvas = document.getElementById("myCanvas");
+        let ctx = canvas.getContext("2d");
+        let currentTool = "none"; // Default: no drawing
+        let currentColor = "#000000"; // Default color: black
+        let isDrawing = false;
+        let startX, startY;
+        let lineWidth = 5; // Default line
         // Event listener for the color picker input
         document.getElementById("colorPicker").addEventListener("input", function (e) {
             currentColor = e.target.value;
         });
-
+        document.getElementById("sizeSlider").addEventListener("input", function (e) {
+            lineWidth = e.target.value;
+        });
         document.getElementById("drawRect").addEventListener("click", function () {
             currentTool = "rect";
         });
@@ -28,18 +31,19 @@
         
 
         canvas.addEventListener("mousedown", function (e) {
-            // Implement logic to draw based on the selected tool and color
+            // Implement qlogic to draw based on the selected tool and color
             ctx.fillStyle = currentColor;
-
+            isDrawing = true;
+            startX = e.clientX - canvas.getBoundingClientRect().left;
+            startY = e.clientY - canvas.getBoundingClientRect().top;
             if (currentTool === "rect") {
                 ctx.fillRect(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top, 50, 50);
             } else if (currentTool === "circle") {
                 ctx.beginPath();
                 ctx.arc(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top, 25, 0, 2 * Math.PI);
                 ctx.fill();
-            } else if (currentTool === "line") {
-                // Implement line drawing logic here
-            } else if (currentTool === "freehandLine") {
+            } 
+            else if (currentTool === "freehandLine") {
                 ctx.beginPath();
                 ctx.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
             }
@@ -48,7 +52,7 @@
             if (isDrawing) {
                 // logic to draw based on the selected tool and color
                 ctx.strokeStyle = currentColor;
-                ctx.lineWidth = 2;
+                ctx.lineWidth = lineWidth;
 
                 if (currentTool === "freehandLine") {
                     ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
@@ -56,11 +60,18 @@
                 }
             }
         });
-        canvas.addEventListener("mouseup", function () {
+        canvas.addEventListener("mouseup", function (e) {
             isDrawing = false;
 
             // Close the path for freehand lines
             if (currentTool === "freehandLine") {
                 ctx.closePath();
+            }
+            if (currentTool === "line") {
+                ctx.strokeStyle = currentColor;
+                ctx.beginPath();
+                ctx.moveTo(startX, startY);
+                ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+                ctx.stroke();
             }
         });
