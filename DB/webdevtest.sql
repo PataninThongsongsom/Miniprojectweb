8c0f236d741f740d1a1fd7f3450894d4d14e4bf8
PataninThongsongsom <s6404062660011@email.kmutnb.ac.th>
2023-09-14 16:27:07 +0700

    add DB file to backup

---
+++ DB/webdevtest.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2023 at 11:26 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webdevtest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_account`
+--
+
+CREATE TABLE `member_account` (
+  `Id` int(255) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `MD_Id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_detail`
+--
+
+CREATE TABLE `member_detail` (
+  `Id` int(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Surname` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `tel` varchar(255) NOT NULL,
+  `Address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `member_account`
+--
+ALTER TABLE `member_account`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `ID` (`MD_Id`);
+
+--
+-- Indexes for table `member_detail`
+--
+ALTER TABLE `member_detail`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `member_account`
+--
+ALTER TABLE `member_account`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `member_detail`
+--
+ALTER TABLE `member_detail`
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `member_account`
+--
+ALTER TABLE `member_account`
+  ADD CONSTRAINT `member_account_ibfk_1` FOREIGN KEY (`MD_Id`) REFERENCES `member_detail` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
