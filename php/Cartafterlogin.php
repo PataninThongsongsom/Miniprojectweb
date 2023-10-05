9a282e65c1ec167842652eb05a39ddf6e49eb2e5
PataninThongsongsom <s6404062660011@email.kmutnb.ac.th>
2023-10-05 17:17:58 +0700

    add cart feture add shop feture can buy item and changes to cart page

--- php/Cartafterlogin.php
+++ php/Cartafterlogin.php
@@ -1,5 +1,47 @@
 <?php
 session_start();
+include "./connect.php";
+if ($_GET["action"]=="add") {
+	
+	$pid = $_GET['pid'];
+    $sql = "SELECT * FROM products WHERE PID = '$pid' ";
+    
+    $result = $con->query($sql);
+    $res = mysqli_fetch_assoc($result);
+	$cart_item = array(
+ 		'pid' => $pid,
+		'pname' => $res["product_name"],
+		'price' => $res["price"],
+		'qty' => $_GET['qty'],
+        'img' => $_GET['img']
+	);
+
+	// ถ้ายังไม่มีสินค้าใดๆในรถเข็น
+	if(empty($_SESSION['cart']))
+    	$_SESSION['cart'] = array();
+ 
+	// ถ้ามีสินค้านั้นอยู่แล้วให้บวกเพิ่ม
+	if(array_key_exists($pid, $_SESSION['cart'])){
+		$_SESSION['cart'][$pid]['qty'] += $_GET['qty'];
+		
+	}
+		
+	// หากยังไม่เคยเลือกสินค้นนั้นจะ
+	else
+	    $_SESSION['cart'][$pid] = $cart_item;
+
+// ปรับปรุงจำนวนสินค้า
+} else if ($_GET["action"]=="update") {
+	$pid = $_GET["pid"];     
+	$qty = $_GET["qty"];
+	$_SESSION['cart'][$pid]['qty'] = $qty;
+
+// ลบสินค้า
+} else if ($_GET["action"]=="delete") {
+	
+	$pid = $_GET['pid'];
+	unset($_SESSION['cart'][$pid]);
+}
 
 
 $user = $_SESSION['user_login'];
@@ -14,7 +56,7 @@ $user = $_SESSION['user_login'];
     <title>Document</title>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script src = "../js/cart2.js"></script>
+    <!-- <script src = "../js/cart2.js"></script> -->
 </head>
 <body>
    
@@ -29,7 +71,7 @@ $user = $_SESSION['user_login'];
         </ul>
         <div class="menu-right">
             <input type="search" class="searchbox" placeholder="Search Products" >
-            <a href="./Cartafterlogin.php"><img src="../img/cart.png" class="cart"></a>
+            <a href="#"><img src="../img/cart.png" class="cart"></a>
             <a href="./login.php"><img src="../img/Login.png" class="login"> </a>
         </div>
     </nav>
@@ -41,35 +83,44 @@ $user = $_SESSION['user_login'];
     
 </div>
 <br>
-
-<div class = "mycart">
-    <div class = "product">
+<?php
+            $sum =0;
+            foreach ($_SESSION["cart"] as $item){
+                $sum += $item["price"] * $item["qty"];
+            ?>
+    <div class = "mycart">
+        
+        <div class = "product">
         <div class = "listcart">
             <div class = "numberlist">
-                <img src = "../img/Product_cloth_dataset/tshirt/1.jpg" class ="numberlist-img">
+                
+                <img src = "<?=$item["img"]?>" class ="numberlist-img">
                 <div class = "numberlist-text">
-                    <p class = "numberlist-name">T-SHIRT</p>
-                    <p class = "numberlist-des">BEN10</p>
+                    
+                    <p class = "numberlist-name"><?=$item["pname"]?></p>
+                    <!-- <p class = "numberlist-des">BEN10</p> -->
                 </div>
                 <div class ="quantity">
                     <button class ="quantity-btn decrease">-</button>
-                    <h3 class ="item-quantity">1</h3>
+                    <h3 class ="item-quantity"><?=$item["qty"]?></h3>
                     <button class = "quantity-btn increase">+</button>
                 </div>
-                <h2 class = "numberlist-price">500 ฿</h2>
-                <button class = "numberlist-del-btn"><ion-icon name="trash-sharp"></ion-icon></button>
+                <h2 class = "numberlist-price"><?=$item["price"]?> ฿</h2>
+                <!-- <button class = "numberlist-del-btn"><ion-icon name="trash-sharp" ></ion-icon></button><br> -->
+                <a href="?action=delete&pid=<?=$item["pid"]?>">ลบ</a>
+                
             </div>
         </div>
-       
-    </div>
+      
+    </div> <?php }?>
     <div class = "subtotal">
         <div class = "subtotal-box">
             <h1 class = "text">YOUR TOTAL</h1>
-            <h2 class = "subtotalprice">500฿</h2>
+            <h2 class = "subtotalprice"><?=$sum?> ฿</h2>
             <a href="#" class="subtotal-btn">BUY NOW!</a>
         </div>
     </div>
-</div>
+</div> 
 <script src = "../js/cart.js"></script>
 
   
