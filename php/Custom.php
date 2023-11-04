4636f43d3c9e390549cb8b8df48097fe753ad7c3
PataninThongsongsom <s6404062660011@email.kmutnb.ac.th>
2023-11-04 17:05:39 +0700

    fix bg of Custom page not changes size

--- php/Custom.php
+++ php/Custom.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['username'])) { // ถ้าไม่ได้เข้า�
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   
-    <link rel="stylesheet" href="../css/styleCustom_2.css">
+    <link rel="stylesheet" href="../css/styleCustom.css">
     <title>CustomYourOwn</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -22,8 +22,8 @@ if (!isset($_SESSION['username'])) { // ถ้าไม่ได้เข้า�
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <canvas id="canvas" width="1920" height="969">
-        </canvas>
+          <canvas id="canvas" style="max-width: 100%; height: auto;">
+          </canvas>
           <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header">
               <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
@@ -120,7 +120,9 @@ if (!isset($_SESSION['username'])) { // ถ้าไม่ได้เข้า�
             </div>
           </div>
         </div>
+        
       </nav>
+      
       <script src="../js/Custom.js"></script>
 </body>
 </html>
